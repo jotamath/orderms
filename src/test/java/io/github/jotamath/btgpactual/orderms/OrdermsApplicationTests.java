264c9fec93e5c4cdbaa9a6ef4be033a7b4d192bc
jotamath <01jmnc@gmail.com>
2024-09-06 23:41:30 -0300

    primeiras configurações de banco de dados com mongo db e mensageria com RabbitMQ, além do uso de Docker para inicialização do projeto em si

---
+++ src/test/java/io/github/jotamath/btgpactual/orderms/OrdermsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jotamath.btgpactual.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
